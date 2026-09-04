97beed3a5b298b55ed50d1562cfb8bbe82a1230a
Gildman Laércio <laerciokarlagabm@gmail.com>
2026-09-03 22:25:43 -0300

    feat: implement daily weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // - selecionar elementos -> testar console.log
 const GEOCODING_BASE_URL = "https://geocoding-api.open-meteo.com/v1/search";
 const WEATHER_BASE_URL = "https://api.open-meteo.com/v1/forecast";
+const ICONS_PATH = "assets/images/";
 
 const unitsButton = document.querySelector(".units__button");
 const unitsMenu = document.querySelector(".units__menu");
@@ -17,7 +18,9 @@ const currentLocation = document.querySelector(".current-location__description")
 const currentTemperature = document.querySelector(".current-temperature__value");
 const currentDate = document.querySelector(".current-date");
 const currentIcon = document.querySelector(".current-temperature__icon");
-const ICONS_PATH = "assets/images/";
+const currentResultValues = document.querySelectorAll(".current-result__value");
+
+const dailyForecastItems = document.querySelectorAll(".daily-forecast__item");
 
 const datetime = new Date();
 const formattedDatetime = datetime.toLocaleDateString('en-US', {
@@ -98,7 +101,7 @@ form.addEventListener("submit", (e) => {
 // - ícone
 
 async function getWeather(latitude, longitude) {
-  const url = `${WEATHER_BASE_URL}?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`;
+  const url = `${WEATHER_BASE_URL}?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code,precipitation&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto`;
 
   try {
     const response = await fetch(url);
@@ -108,19 +111,116 @@ async function getWeather(latitude, longitude) {
     }
 
     const data = await response.json();
+
     const temperature = data.current.temperature_2m;
     const humidity = data.current.relative_humidity_2m;
     const wind = data.current.wind_speed_10m;
     const weatherCode = data.current.weather_code;
+    const precipitation = data.current.precipitation;
 
+    const temperatureMin = data.daily.temperature_2m_min;
+    const temperatureMax = data.daily.temperature_2m_max;
+    const dailyDates = data.daily.time;
+    const dailyWeatherCodes = data.daily.weather_code;
+
+    console.log(url);
     // console.log(`Temperature: ${temperature}ºC`);
-    // console.log(url);
-    console.log(`Humidity: ${humidity}%`);
-    console.log(`Wind: ${wind} km/h`);
-    console.log(`Weather Code: ${weatherCode}`);
+    // console.log(`Humidity: ${humidity}%`);
+    // console.log(`Wind: ${wind} km/h`);
+    // console.log(`Weather Code: ${weatherCode}`);
+    // console.log(`Precipitation: ${precipitation} mm`);
+    console.log(dailyDates);
+    console.log(temperatureMin);
+    console.log(temperatureMax);
+    console.log(dailyWeatherCodes);
 
     currentDate.textContent = formattedDatetime;
     currentTemperature.textContent = `${temperature}°`;
+    currentResultValues[0].textContent = `${temperature}°`;
+    currentResultValues[1].textContent = `${humidity}%`;
+    currentResultValues[2].textContent = `${wind} km/h`;
+    currentResultValues[3].textContent = `${precipitation} mm`;
+
+    for (let i=0; i < dailyForecastItems.length; i++) {
+      const item = dailyForecastItems[i];
+      const date = new Date(`${dailyDates[i]}T00:00:00`);
+      const formattedDay = date.toLocaleDateString('en-US', {
+      weekday: 'short'
+    });
+
+      const weatherCode = dailyWeatherCodes[i];
+      const dailyIcon = item.querySelector(".daily-forecast__icon");
+      
+    console.log(dailyDates[i], formattedDay);
+
+      switch (weatherCode) {
+        case 0:
+        case 1:
+          // sunny 0 1
+          dailyIcon.src = `${ICONS_PATH}icon-sunny.webp`;
+          break;
+          
+        case 2:
+          // partly-cloudy 2
+          dailyIcon.src = `${ICONS_PATH}icon-partly-cloudy.webp`;
+          break;
+          
+        case 3:
+          // overcast 3
+          dailyIcon.src = `${ICONS_PATH}icon-overcast.webp`;
+          break;
+
+        case 45:
+          // fog 45
+          dailyIcon.src = `${ICONS_PATH}icon-fog.webp`;
+          break;
+        
+        case 51:
+        case 53:
+        case 55:
+        case 56:
+        case 57:
+          // drizzle 51 53 55 56 57
+          dailyIcon.src = `${ICONS_PATH}icon-drizzle.webp`;
+          break;
+
+        case 61:
+        case 63:
+        case 65:
+        case 66:
+        case 67:
+        case 80:
+        case 81:
+        case 82:
+          // rain 61 63 65 66 67 80 81 82
+          dailyIcon.src = `${ICONS_PATH}icon-rain.webp`;
+          break;
+
+        case 71:
+        case 73:
+        case 75:
+        case 77:
+        case 85:
+        case 86:
+          // snow 71 73 75 77 85 86
+          dailyIcon.src = `${ICONS_PATH}icon-snow.webp`;
+          break;
+
+        case 95:
+        case 96:
+        case 99:
+          // storm 95 96 99
+          dailyIcon.src = `${ICONS_PATH}icon-storm.webp`;
+          break;
+          
+        default:
+          dailyIcon.src = `${ICONS_PATH}icon-sunny.webp`;
+      }
+      
+      item.querySelector(".daily-forecast__day").textContent = formattedDay;
+      item.querySelector(".daily-forecast__temperature-max").textContent = `${temperatureMax[i]}`;
+      item.querySelector(".daily-forecast__temperature-min").textContent = `${temperatureMin[i]}`;
+    }
 
     switch (weatherCode) {
       case 0:
@@ -214,11 +314,11 @@ async function searchCity (city) {
     const latitude = data.results[0].latitude;
     const longitude = data.results[0].longitude;
 
-    currentLocation.textContent = `${name}, ${country}`;
-    // console.log(`City: ${name}`);
     console.log(url);
+    // console.log(`City: ${name}`);
     // console.log(`Latitude: ${latitude}`);
     // console.log(`Longitude: ${longitude}`);
+    currentLocation.textContent = `${name}, ${country}`;
 
     await getWeather(latitude, longitude);
   } catch (error) {
