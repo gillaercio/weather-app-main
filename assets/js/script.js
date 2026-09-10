b6dc43ae8cb1e2a1e9f34123d0fe77d771b5fd46
Gildman Laércio <laerciokarlagabm@gmail.com>
2026-09-10 17:17:54 -0300

    fix: z-index, hours/units dropdowns and input style fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,10 @@ const currentIcon = document.querySelector(".current-temperature__icon");
 const currentResultValues = document.querySelectorAll(".current-result__value");
 
 const dailyForecastItems = document.querySelectorAll(".daily-forecast__item");
+
+const hourlyForecastBtn = document.querySelector(".hourly-forecast__button");
+const hourlyForecastDayText = document.querySelector(".hourly-forecast__day");
+const hourlyDayMenu = document.querySelector("#hourly-day-menu");
 const hourlyList = document.querySelector(".hourly-forecast__list");
 
 const datetime = new Date();
@@ -31,18 +35,23 @@ const formattedDatetime = datetime.toLocaleDateString('en-US', {
   year: 'numeric'
 });
 
-// let selectedTemperature = "Celsius (°C)";
-// let selectedWind = "km/h";
-// let selectedPrecipitation = "Millimeters (mm)";
-
 let isImperial = false;
 
 // Etapa JS 2 — Menu de unidades
 // - abrir
 // - fechar
 // - selecionar
-unitsButton.addEventListener("click", () => {
-  unitsMenu.classList.toggle('hidden');
+unitsButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const isHidden = unitsMenu.hasAttribute("hidden");
+
+  if (isHidden) {
+    unitsMenu.removeAttribute("hidden");
+    unitsButton.setAttribute("aria-expanded", "true");
+  } else {
+    unitsMenu.setAttribute("hidden");
+    unitsButton.setAttribute("aria-expanded", "false");
+  }
 })
 
 unitsToggle.addEventListener("click", () => {
@@ -53,33 +62,46 @@ unitsToggle.addEventListener("click", () => {
   saveSelectedPrecipitation();
 })
 
+hourlyForecastBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const isHidden = hourlyDayMenu.hidden;
+
+  if (isHidden) {
+    hourlyDayMenu.removeAttribute("hidden");
+    hourlyForecastBtn.setAttribute("aria-expanded", "true");
+  } else {
+    hourlyDayMenu.setAttribute("hidden", "");
+    hourlyForecastBtn.setAttribute("aria-expanded", "false");
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!unitsButton.contains(e.target) && !unitsMenu.contains(e.target)) {
+    unitsMenu.classList.add("hidden");
+    unitsButton.setAttribute("aria-expanded", "false");
+  }
+
+  if (!hourlyForecastBtn.contains(e.target) && !hourlyDayMenu.contains(e.target)) {
+    hourlyDayMenu.hidden = true;
+    hourlyForecastBtn.setAttribute("aria-expanded", "false");
+  }
+})
+
 function saveSelectedTemperature() {
   temperatures.forEach(temperature => {
-    if (temperature.value === "fahrenheit") {
-      temperature.checked = isImperial;
-    } else {
-      temperature.checked = !isImperial;
-    }
+    temperature.checked = (temperature.value === "fahrenheit") ? isImperial : !isImperial;
   });
 }
 
 function saveSelectedWind() {
   winds.forEach(wind => {
-    if (wind.value === "mph") {
-      wind.checked = isImperial;
-    } else {
-      wind.checked = !isImperial;
-    }
+    wind.checked = (wind.value === "mph") ? isImperial : !isImperial;
   });
 }
 
 function saveSelectedPrecipitation() {
   precipitations.forEach(precipitation => {
-    if (precipitation.value === "in") {
-      precipitation.checked = isImperial;
-    } else {
-      precipitation.checked = !isImperial;
-    }
+    precipitation.checked = (precipitation.value === "in") ? isImperial : !isImperial;
   });
 }
 
@@ -100,6 +122,9 @@ form.addEventListener("submit", (e) => {
 // - data
 // - temperatura
 // - ícone
+
+// Etapa JS 6 — cards
+// - current results -> daily forecast -> hourly forecast
 
 function getWeatherIcon(code) {
   switch (code) {
@@ -142,6 +167,50 @@ function getWeatherIcon(code) {
   }
 }
 
+function renderHourlyForecast(dayIndex, data) {
+  hourlyList.innerHTML = "";
+
+  const hourlyTimes = data.hourly.time;
+  const hourlyTemperatures = data.hourly.temperature_2m;
+  const hourlyWeatherCodes = data.hourly.weather_code;
+
+  let startHourIndex = dayIndex * 24;
+  let endHourIndex = startHourIndex + 24;
+
+  if(dayIndex === 0) {
+    const currentHour = new Date().getHours();
+    const foundIndex = hourlyTimes.findIndex(time => new Date(time).getHours() === currentHour);
+    if (foundIndex !== -1) {
+      startHourIndex = foundIndex;
+    }
+  }
+
+  for (let i = startHourIndex; i < endHourIndex; i++) {
+    const timeString = hourlyTimes[i];
+    const temp = Math.round(hourlyTemperatures[i]);
+    const code = hourlyWeatherCodes[i];
+
+    const dateObj = new Date(timeString);
+    const formattedHour = dateObj.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      hour12: true
+    });
+
+    const li = document.createElement("li");
+    li.classList.add("hourly-forecast__item");
+
+    li.innerHTML = `
+      <div class="hourly-forecast__sub-item">
+        <img src="${getWeatherIcon(code)}" alt="" class="hourly-forecast__icon">
+        <span class="hourly-forecast__time">${formattedHour}</span>
+      </div>
+      <span class="hourly-forecast__temperature">${temp}°</span>
+    `;
+
+    hourlyList.appendChild(li);
+  }
+}
+
 async function getWeather(latitude, longitude) {
   const url = `${WEATHER_BASE_URL}?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code,precipitation&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto&hourly=temperature_2m,weather_code`;
 
@@ -154,106 +223,58 @@ async function getWeather(latitude, longitude) {
 
     const data = await response.json();
 
-    const temperature = data.current.temperature_2m;
-    const humidity = data.current.relative_humidity_2m;
-    const wind = data.current.wind_speed_10m;
-    const weatherCode = data.current.weather_code;
-    const precipitation = data.current.precipitation;
-
-    const temperatureMin = data.daily.temperature_2m_min;
-    const temperatureMax = data.daily.temperature_2m_max;
-    const dailyDates = data.daily.time;
-    const dailyWeatherCodes = data.daily.weather_code;
-    const hourlyTimes = data.hourly.time;
-    const hourlyTemperatures = data.hourly.temperature_2m;
-    const hourlyWeatherCodes = data.hourly.weather_code;
-    const currentHour = new Date().getHours();
-    // const firstHourlyItem = document.querySelector(".hourly-forecast__item");
-    // const firstHourlyItem = document.querySelector(".hourly-forecast__time");
-
-    const currentHourIndex = hourlyTimes.findIndex(time => {
-      return new Date(time).getHours() === currentHour;
-    });
-
-    // console.log(url);
-    // console.log(`Temperature: ${temperature}ºC`);
-    // console.log(`Humidity: ${humidity}%`);
-    // console.log(`Wind: ${wind} km/h`);
-    // console.log(`Weather Code: ${weatherCode}`);
-    // console.log(`Precipitation: ${precipitation} mm`);
-    // console.log(dailyDates);
-    // console.log(temperatureMin);
-    // console.log(temperatureMax);
-    // console.log(dailyWeatherCodes);
-    // console.log(data.hourly);
-    // console.log(hourlyTimes);
-    // console.log(hourlyTemperatures);
-    // console.log(hourlyWeatherCodes);
-    // console.log(currentHour);
-    // console.log(currentHourIndex);
-    // console.log("Horário: "+hourlyTimes[currentHourIndex]);
-    // console.log("Temperatura: "+hourlyTemperatures[currentHourIndex]);
-    // console.log(hour);
-
     currentDate.textContent = formattedDatetime;
-    currentTemperature.textContent = `${temperature}°`;
-    currentResultValues[0].textContent = `${temperature}°`;
-    currentResultValues[1].textContent = `${humidity}%`;
-    currentResultValues[2].textContent = `${wind} km/h`;
-    currentResultValues[3].textContent = `${precipitation} mm`;
-    // firstHourlyItem.textContent = hourlyTimes[currentHourIndex].slice(11,13);
-    // const hour = hourlyTimes[currentHourIndex].slice(11,13);
-    // const hourNumber = Number(hour);
-    // firstHourlyItem.textContent = hourNumber;
-    // console.log(hourNumber);
-    // firstHourlyItem.textContent = formattedHour;
+    currentTemperature.textContent = `${data.current.temperature_2m}°`;
+    currentResultValues[0].textContent = `${data.current.temperature_2m}°`;
+    currentResultValues[1].textContent = `${data.current.relative_humidity_2m}%`;
+    currentResultValues[2].textContent = `${data.current.wind_speed_10m} km/h`;
+    currentResultValues[3].textContent = `${data.current.precipitation} mm`;
+    currentIcon.src = getWeatherIcon(data.current.weather_code);
 
-    currentIcon.src = getWeatherIcon(weatherCode);
+    renderHourlyForecast(0, data);
 
-    hourlyList.innerHTML = "";
-    for (let i = currentHourIndex; i < 24; i++) {
-      const timeString = hourlyTimes[i];
-      const temp = Math.round(hourlyTemperatures[i]);
-      const code = hourlyWeatherCodes[i];
+    hourlyDayMenu.innerHTML = "";
+    hourlyDayMenu.setAttribute("hidden", "");
+    hourlyForecastBtn.setAttribute("aria-expanded", "false");
 
-      const dateObj = new Date(timeString);
-      const formattedHour = dateObj.toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        hour12: true
+    for (let i = 0; i < dailyForecastItems.length; i++) {
+      const item = dailyForecastItems[i];
+      const date = new Date(`${data.daily.time[i]}T00:00:00`);
+
+      const formattedDayShort = date.toLocaleDateString('en-US', {
+        weekday: 'short'
+      });
+      const formattedDayLong = date.toLocaleDateString('en-US', {
+        weekday: 'long'
       });
 
-      const li = document.createElement("li");
-      li.classList.add("hourly-forecast__item");
+      item.querySelector(".daily-forecast__icon").src = getWeatherIcon(data.daily.weather_code[i]);
+      item.querySelector(".daily-forecast__day").textContent = formattedDayShort;
+      item.querySelector(".daily-forecast__temperature-max").textContent = `${data.daily.temperature_2m_max[i]}°`;
+      item.querySelector(".daily-forecast__temperature-min").textContent = `${data.daily.temperature_2m_min[i]}°`;
 
-      li.innerHTML = `
-        <div class="hourly-forecast__sub-item">
-          <img src="${getWeatherIcon(code)}" alt="" class="hourly-forecast__icon">
-          <span class="hourly-forecast__time">${formattedHour}</span>
-        </div>
-        <span class="hourly-forecast__temperature">${temp}°</span>
-      `;
+      if (i === 0) {
+        hourlyForecastDayText.textContent = formattedDayLong;
+      }
 
-      hourlyList.appendChild(li);
+      const optionBtn = document.createElement("button");
+      optionBtn.type = "button";
+      optionBtn.classList.add("hourly-forecast__option");
+      optionBtn.textContent = formattedDayLong;
+
+      optionBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        hourlyForecastDayText.textContent = formattedDayLong;
+        renderHourlyForecast(i, data);
+        
+        hourlyDayMenu.setAttribute("hidden", "");
+        hourlyForecastBtn.setAttribute("aria-expanded", "false");
+      });
+
+      hourlyDayMenu.appendChild(optionBtn);
     }
-    
-    for (let i=0; i < dailyForecastItems.length; i++) {
-      const item = dailyForecastItems[i];
-      const date = new Date(`${dailyDates[i]}T00:00:00`);
-      const formattedDay = date.toLocaleDateString('en-US', {
-      weekday: 'short'
-    });
 
-      const weatherCode = dailyWeatherCodes[i];
-      const dailyIcon = item.querySelector(".daily-forecast__icon");
-
-      dailyIcon.src = getWeatherIcon(weatherCode);
-      
-      item.querySelector(".daily-forecast__day").textContent = formattedDay;
-      item.querySelector(".daily-forecast__temperature-max").textContent = `${temperatureMax[i]}`;
-      item.querySelector(".daily-forecast__temperature-min").textContent = `${temperatureMin[i]}`;
-    }
-    
-    return {temperature, humidity, wind};
+    return {data};
   } catch (error) {
     console.error("An error occurred while fetching the data:", error.message);
   }
@@ -281,11 +302,6 @@ async function searchCity (city) {
     const latitude = data.results[0].latitude;
     const longitude = data.results[0].longitude;
 
-    // console.log(url);
-    // console.log(data.results);
-    // console.log(`City: ${name}`);
-    // console.log(`Latitude: ${latitude}`);
-    // console.log(`Longitude: ${longitude}`);
     currentLocation.textContent = `${name}, ${country}`;
 
     await getWeather(latitude, longitude);
@@ -293,9 +309,6 @@ async function searchCity (city) {
     console.error("An error occurred while fetching the data:", error.message);
   }
 }
-
-// Etapa JS 6 — cards
-// - current results -> daily forecast -> hourly forecast
 
 // Etapa JS 7 — estados
 // - loading
